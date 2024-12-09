fe8a7bea797111f20129f33ec8dd89679c5d9b0b
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-12-09 03:43:21 -0500

    Initialize Repo for LazyVim Config

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    on_colors = function(colors)
+      colors.border = "#565f89"
+    end,
+  },
+}
