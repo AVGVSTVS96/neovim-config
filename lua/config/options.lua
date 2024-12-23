7e83941fffcc6b5a6e8a14cd340a62cd6f1cf215
Bassim Shahidy <bassim101@gmail.com>
2024-12-22 20:18:26 -0500

    feat: Set mousescroll step for MacOS only

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,9 +3,15 @@
 -- Add any additional options here
 local opt = vim.opt
 local g = vim.g
+local o = vim.o
 
 opt.listchars = "eol:↲,tab:|->,lead:·,space: ,extends:→,precedes:←,nbsp:␣"
 opt.breakindent = true
 opt.linebreak = true
 
 g.snacks_animate = false
+
+-- MacOS specific options
+if jit.os == "OSX" then
+  o.mousescroll = "ver:1"
+end
